97aa6fa0d0d4e382c274015bbcd7591f9c02c5cf
root <root@nbmkemao220.MKMONDIAL.local>
2024-06-18 15:17:28 -0400

    update

--- server/src/models/eletrocheck-shoes/getCollaborators.ts
+++ server/src/models/eletrocheck-shoes/getCollaborators.ts
@@ -3,13 +3,13 @@ import { db } from "../../config/db";
 // para selecionar todos os colaboradores
 export const getAllCollaborators = async () => {
     try {
-        return new Promise (async(resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             const q = 'SELECT id, name FROM colab'
             await db.query(q, (err, data) => {
                 if (err) {
                     reject(err)
                 }
-                if (data && data.length > 0 ) {
+                if (data && data.length > 0) {
                     resolve(data)
                 }
             })
@@ -21,13 +21,13 @@ export const getAllCollaborators = async () => {
 
 export const getCollaboratorForId = async (id: string) => {
     try {
-        return new Promise (async(resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             const q = 'SELECT id, name FROM colab WHERE id = ?'
             await db.query(q, [id], (err, data) => {
                 if (err) {
                     reject(err)
                 }
-                if (data && data.length > 0 ) {
+                if (data && data.length > 0) {
                     resolve(data)
                 }
             })
@@ -38,17 +38,19 @@ export const getCollaboratorForId = async (id: string) => {
 }
 
 // para selecionar todos os que fizeram o teste no mês
-export const getDoneTests = async ( date:  string ) => {
+export const getDoneTests = async (date: string) => {
     try {
-        return new Promise (async(resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             const q = "SELECT registration, result FROM results WHERE date LIKE ?"
             await db.query(q, [date], (err, data) => {
                 if (err) {
                     reject(err)
                 }
                 if (data && data.length > 0) {
-                    const hashTable = data.reduce((acc: { [key: string]: string }, value: { registration: string, result: string }) => {
-                        acc[value.registration] = value.result;
+                    const hashTable = data.reduce((acc: { [key: string]: { result: string, id: number } }, value: { registration: string, result: string, id: number }) => {
+                        if (!acc[value.registration] || acc[value.registration].id < value.id) {
+                            acc[value.registration] = { result: value.result, id: value.id };
+                        }
                         return acc;
                     }, {});
                     resolve(hashTable);
